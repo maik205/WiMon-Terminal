70fcd37dbe5238b28351a5b6a053cee93b60d0d0
Maik <maikpla3@gmail.com>
2025-03-04 11:27:13 +0700

    Refactored and added licensing info

---
+++ data.h
@@ -0,0 +1,4 @@
+/**
+  COPYRIGHT - 2025 - THE WIMON TEAM
+  Contains references to code written by Rui Santos & Sara Santos - Random Nerd Tutorials
+*/
--- screen.h
+++ screen.h
@@ -1,5 +1,17 @@
+/**
+  COPYRIGHT - 2025 - THE WIMON TEAM
+  Contains references to code written by Rui Santos & Sara Santos - Random Nerd Tutorials
+*/
 #pragma once
 
+#include <lvgl.h>
+#include <lvgl.h>
+#include <TFT_eSPI.h>
+#include <XPT2046_Touchscreen.h>
+
+// Remember to set logging level to 0 for prod.
+//TODO: get actual pin out for TFT and Touch Controller
+// Touch panel specific pin
 #define TOUCH_IRQ 17
 #define TOUCH_CS 21  // Chip select pin (T_CS) of touch screen
 
@@ -15,3 +27,10 @@
 #define SCREEN_WIDTH 320
 #define SCREEN_HEIGHT 480
 #define FONT_SIZE 2
+
+void wm_init_screen(void);
+
+void lv_create_main_gui(void);
+void lv_touch_read(lv_indev_t* touchscreen, lv_indev_data_t* data);
+void lv_log_cb(lv_log_level_t level, const char* buf);
+
